14a58f7cfbf86d5dc9c7ddd1bf7666a4cec611b3
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-29 15:15:50 +0200

    update main method of Game module

---
+++ frontend/js/Game.js
@@ -0,0 +1,30 @@
+import Player from './Player.js';
+import { loadImage } from './loaders.js';
+
+class Game {
+  constructor() {
+    this.gameCanvas = document.getElementById('game');
+    this.gameCanvas.width = window.innerWidth;
+    this.gameCanvas.height = window.innerHeight;
+    this.gameCanvasContext = this.gameCanvas.getContext('2d');
+  }
+  clearBackground() {
+    this.gameCanvasContext.fillRect(
+      0,
+      0,
+      this.gameCanvas.width,
+      this.gameCanvas.height
+    );
+    // requestAnimationFrame(this.clearBackground.bind(this));
+  }
+  start() {
+    loadImage('./frontend/img/cn.png')
+    .then(spriteImage => {
+      const player = new Player("Silent Bob", this.gameCanvasContext, spriteImage);
+      // this.clearBackground();
+      [player].forEach(entity => entity.update());
+    });
+  }
+}
+
+export default Game;
