2e84ca9285f3e5ae2ec9d6c45b3ee0a07425d765
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-30 01:27:36 +0200

    refactor keyboard input

--- frontend/js/controller/Controller.js
+++ frontend/js/controller/Controller.js
@@ -1,31 +1,39 @@
-const KEY_DOWN = 'keydown';
-const KEY_UP = 'keyup';
+const EVENT_KEY_DOWN = 'keydown';
+const EVENT_KEY_UP = 'keyup';
 const KEY_A = 'a';
 const KEY_D = 'd';
 const KEY_W = 'w';
 const KEY_S = 's';
 
 class Controller {
-  constructor() {
+  constructor(entity) {
+    this.entity = entity;
     this.currentKey = {};
     this.dir = 0;
-    [KEY_DOWN, KEY_UP].forEach(eventName => {
+    this.handleKeyboardEvents([EVENT_KEY_DOWN, EVENT_KEY_UP]);
+  }
+  handleKeyboardEvents(keyboardEvents) {
+    keyboardEvents.forEach(eventName => {
       window.addEventListener(eventName, event => {
-        if(eventName == KEY_DOWN) {
+        this.currentEventName = eventName;
+        if(eventName == EVENT_KEY_DOWN) {
           switch(event.key) {
             case KEY_D:
-            case KEY_S:
-              this.dir = 1; break;
+            // case KEY_S:
+              this.toggleProp('dir', 1, 'E'); break;
             case KEY_A:
-            case KEY_W:
-              this.dir = -1; break;
+            // case KEY_W:
+              this.toggleProp('dir', -1, 'W'); break;
           }
           return this.currentKey[event.key] = true;
         }
         return this.currentKey[event.key] = false;
       });
-
     });
+  }
+  toggleProp(prop, value, orientation) {
+    this.entity.orientation = orientation;
+    this[prop] = value;
   }
 }
 
